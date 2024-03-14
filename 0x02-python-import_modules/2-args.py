a4e29d9173ad0cbc9eb9f9582f859f1ab44b901e
stephen <muruchistephen8@gmail.com>
2024-03-14 08:07:28 -0700

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
 
-import sys
-
-n = len(sys.argv[1:])
-if n == 1:
-    print(f"{n} argument:")
-else:
-    print(f"{n} arguments:")
-for i, arg in enumerate(sys.argv[1:]):
-    print(f"{i}: {arg}")
+    n = len(sys.argv[1:])
+    if n == 1:
+        print(f"{n} argument:")
+    else:
+        print(f"{n} arguments:")
+    for i, arg in enumerate(sys.argv[1:]):
+        print(f"{i}: {arg}")
