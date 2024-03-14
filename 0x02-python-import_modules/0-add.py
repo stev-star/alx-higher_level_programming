da6d500465d9ab8ecec3728c2e7cf851b1021537
stephen <muruchistephen8@gmail.com>
2024-03-14 06:13:08 -0700

    add

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+# import the module required
+    import add_0.py as add
+
+    a = 1
+    b = 2
+# generate the required results
+result = add(a,b)
+
+# print the results
+print(f"{a} + {b} = {result}")
