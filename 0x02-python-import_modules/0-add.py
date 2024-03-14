4753a85929d33f0928a89e15a163503af95e207d
stephen <muruchistephen8@gmail.com>
2024-03-14 06:32:06 -0700

    add updates

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-# import the module required
+    # import the module required
     from add_0 import add
 
     a = 1
     b = 2
 # generate the required results
-result = add(a,b)
+result = add(a, b)
 
 # print the results
 print("{:d} + {:d} = {:d}".format(a, b, result))
