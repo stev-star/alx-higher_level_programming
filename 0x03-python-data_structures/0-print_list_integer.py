4558c1ae9bd3ac96f23c3e14b31b5fe58025f455
stephen <muruchistephen8@gmail.com>
2024-03-18 00:57:22 -0700

    integer print

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,7 +2,7 @@
 def print_list_integer(my_list=[]):
     # loop through each value in my_list
     for x in my_list:
-        print("{}".format(x))
+        print("{:d}".format(x))
 
 
 my_list = [1, 2, 3, 4, 5]
